e40d699081220d520af2934f9c0e8a33f2a53201
jjeroam <jjjmmmdddggg@gmail.com>
2025-10-19 21:14:31 +0800

    just added all files

---
+++ app.js
@@ -0,0 +1,9 @@
+let x = prompt("Enter the value of x: ");
+let y = prompt("Enter the value of y: ");
+let operation = prompt("Choose if you want to +, -, x, /: ");
+
+console.log("Enter the value of x: " + x);
+console.log("Enter the value of y: " + y);
+console.log("Choose if you want to +, -, x, /: " + operation);
+
+console.log(x + operation + y);
