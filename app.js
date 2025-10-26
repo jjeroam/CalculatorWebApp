7d4809c8e6c9d683fb652f1d5d20bb8062098a27
jjeroam <jjjmmmdddggg@gmail.com>
2025-10-26 19:45:34 +0800

    can now input number

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-let x = prompt("Enter the value of x: ");
-let y = prompt("Enter the value of y: ");
-let operation = prompt("Choose if you want to +, -, x, /: ");
+function inputNumber(num) {
+  const display = document.getElementById("number");
+  display.value += num;
+}
 
-console.log("Enter the value of x: " + x);
-console.log("Enter the value of y: " + y);
-console.log("Choose if you want to +, -, x, /: " + operation);
+function clearDisplay() {
+  document.getElementById("number").value = "";
+}
 
-console.log(x + operation + y);
+function deleteNumber(char) {
+  const display = document.getElementById("number");
+  display.value -= char;
+}
